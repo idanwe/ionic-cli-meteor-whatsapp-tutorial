c804419c0bb37911d59d3eb20d6be367af0b7935
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-06 14:44:04 -0400

    Step 1.16: Move chat detail to its own file and update index.html

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('whatsapp', ['ionic', 'whatsapp.controllers', 'whatsapp.services'])
+angular.module('whatsapp', ['ionic', 'whatsapp.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
--- www/js/controllers.js
+++
@@ -1,5 +0,0 @@
-angular.module('whatsapp.controllers', [])
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-});
