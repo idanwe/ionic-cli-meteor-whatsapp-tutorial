ddda0e3b729a14a63be79cf755cac448baa592b5
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-06 14:52:20 -0400

    Step 1.19: Move config.js to it's own file and update index.html

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,4 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('whatsapp', ['ionic', 'whatsapp.services'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-});
+angular.module('whatsapp', ['ionic', 'whatsapp.services']);
