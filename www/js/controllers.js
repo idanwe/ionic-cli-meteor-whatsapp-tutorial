c0fb1da3eb8c4dec0558bf1280adbde894210ebd
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-06 13:47:52 -0400

    Step 1.2: Change app's name from `starter` to `whatsapp`

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('whatsapp.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('whatsapp.services', [])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
