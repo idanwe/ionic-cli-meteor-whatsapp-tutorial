a7be67b3055f07af3266511849f43b4d7cfcadf1
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-07 00:21:06 -0400

    Step 3.15: Add controller logic for sending message

--- www/js/controllers/chat-detail.controller.js
+++ www/js/controllers/chat-detail.controller.js
@@ -2,17 +2,33 @@ angular
   .module('whatsapp')
   .controller('ChatDetailCtrl', ChatDetailCtrl);
 
-function ChatDetailCtrl ($scope, $stateParams, $timeout, $ionicScrollDelegate) {
+function ChatDetailCtrl ($scope, $stateParams, $timeout, $ionicScrollDelegate, $meteor) {
   $scope.chat = $scope.$meteorObject(Chats, $stateParams.chatId, false);
 
   $scope.messages = $scope.$meteorCollection(function () {
     return Messages.find({ chatId: $stateParams.chatId });
   }, false);
 
+  $scope.data = {};
+
+  $scope.sendMessage = sendMessage;
   $scope.inputUp = inputUp;
   $scope.inputDown = inputDown;
 
   ////////////
+
+  function sendMessage () {
+    if (_.isEmpty($scope.data.message)) {
+      return;
+    }
+
+    $meteor.call('newMessage', {
+      text: $scope.data.message,
+      chatId: $stateParams.chatId
+    });
+
+    delete $scope.data.message;
+  }
 
   function inputUp () {
     $timeout(function() {
