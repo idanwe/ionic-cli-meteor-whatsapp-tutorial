96c6e9001cacf1b799755029f29c884a0739c68a
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-07 00:07:19 -0400

    Step 3.13: Scroll chat details view according to keyboard open or closed

--- www/js/controllers/chat-detail.controller.js
+++ www/js/controllers/chat-detail.controller.js
@@ -2,10 +2,25 @@ angular
   .module('whatsapp')
   .controller('ChatDetailCtrl', ChatDetailCtrl);
 
-function ChatDetailCtrl ($scope, $stateParams) {
+function ChatDetailCtrl ($scope, $stateParams, $timeout, $ionicScrollDelegate) {
   $scope.chat = $scope.$meteorObject(Chats, $stateParams.chatId, false);
 
   $scope.messages = $scope.$meteorCollection(function () {
     return Messages.find({ chatId: $stateParams.chatId });
   }, false);
+
+  $scope.inputUp = inputUp;
+  $scope.inputDown = inputDown;
+
+  ////////////
+
+  function inputUp () {
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown () {
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
 }
