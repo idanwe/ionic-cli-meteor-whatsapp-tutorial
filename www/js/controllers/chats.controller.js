ae85e5566f01adb8d5d9db58d0c3dfaf74852fc9
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-09 16:51:00 -0400

    Step 5.2: Add create new chat function to chats controller

--- www/js/controllers/chats.controller.js
+++ www/js/controllers/chats.controller.js
@@ -2,12 +2,27 @@ angular
   .module('whatsapp')
   .controller('ChatsCtrl', ChatsCtrl);
 
-function ChatsCtrl ($scope) {
+function ChatsCtrl ($scope, $ionicModal) {
   $scope.chats = $scope.$meteorCollection(Chats, false);
 
+  $ionicModal.fromTemplateUrl('templates/new-chat.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.$on('$destroy', function () {
+    $scope.modal.remove();
+  });
+
+  $scope.openNewChatModal = openNewChatModal;
   $scope.remove = remove;
 
   ////////////
+
+  function openNewChatModal () {
+    $scope.modal.show();
+  }
 
   function remove (chat) {
     $scope.chats.remove(chat);
