548ad90755c1c5646075b3252e82d24eeb66cfaa
Uri Goldshtein <uri.goldshtein@gmail.com>
2015-10-08 22:29:58 -0400

    Step 4.18: Add updateName methods to server

--- server/methods.js
+++ server/methods.js
@@ -10,5 +10,14 @@ Meteor.methods({
     var messageId = Messages.insert(message);
     Chats.update(message.chatId, { $set: { lastMessage: message } });
     return messageId;
+  },
+
+  updateName: function (name) {
+    check(name, String);
+    if (name.length === 0) {
+      throw Meteor.Error('name-required', 'Must provide user name');
+    }
+
+    return Meteor.users.update(this.userId, { $set: { 'profile.name': name } });
   }
 });
