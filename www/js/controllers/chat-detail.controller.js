5b43c8f58852b90a5d318257047dab1ee6d385b1
Idan Wender <idanwe2@gmail.com>
2015-12-08 00:21:20 +0200

    Step 3.9: Bind Messages to the Messages Meteor Collection

--- www/js/controllers/chat-detail.controller.js
+++ www/js/controllers/chat-detail.controller.js
@@ -6,6 +6,9 @@ function ChatDetailCtrl ($scope, $stateParams) {
   $scope.helpers({
     chat: function () {
       return Chats.findOne($stateParams.chatId);
+    },
+    messages: function () {
+      return Messages.find({ chatId: $stateParams.chatId });
     }
   });
 }
